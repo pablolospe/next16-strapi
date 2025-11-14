9d691a9b82e5b8a13252f7944bf9fc2748e053ca
pablolospe <yosoypxl@gmail.com>
2025-11-14 15:21:44 -0300

    cook

--- frontend/actions/auth.ts
+++ frontend/actions/auth.ts
@@ -1,59 +1,58 @@
-'use server'
+"use server"
+import { z } from "zod"
 
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { registerUserService } from "@/lib/strapi";
-import { type FormState, SignupFormSchema } from "@/validations/auth";
-import { z } from "zod";
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+
+import { SignupFormSchema, type FormState } from "@/validations/auth"
+import { registerUserService } from "@/lib/strapi"
 
 const cookieConfig = {
-    maxAge: 60 * 60 * 24 * 7,
-    path: '/',
-    httpOnly: true,
-    domain: process.env.STRAPI_BASE_URL ?? 'localhost',
-    secure: process.env.NODE_ENV === 'production',
+  maxAge: 60 * 60 * 24 * 7, // 1 week,
+  path: '/',
+  httpOnly: true, // only accessible by the server
+  domain: process.env.HOST ?? 'localhost',
+  secure: process.env.NODE_ENV === 'production',
 }
 
-
 export async function registerUserAction(prevState: FormState, formData: FormData): Promise<FormState> {
-    console.log('Hello for Register User action');
+  console.log('Hello from Register User Action')
 
-    const fields = {
-        username: formData.get('username') as string,
-        email: formData.get('email') as string,
-        password: formData.get('password') as string,
-    };
+  const fields = {
+    username: formData.get('username') as string,
+    password: formData.get('password') as string,
+    email: formData.get('email') as string,
+  }
 
-    const validatedFields = SignupFormSchema.safeParse(fields)
+  const validatedFields = SignupFormSchema.safeParse(fields)
 
-    if (!validatedFields.success) {
-        const flattenedErrors = z.flattenError(validatedFields.error)
+  if (!validatedFields.success) {
+    const flattenedErrors = z.flattenError(validatedFields.error)
 
-        console.log('Validation errors:', flattenedErrors.fieldErrors);
+    console.log("Validation errors:", flattenedErrors.fieldErrors)
 
-        return {
-            success: false,
-            message: 'Validation error',
-            strapiErrors: null,
-            zodErrors: flattenedErrors.fieldErrors,
-            data: fields,
-        }
+    return {
+      success: false,
+      message: "Validation error",
+      strapiErrors: null,
+      zodErrors: flattenedErrors.fieldErrors,
+      data: fields
     }
+  }
 
-    const response = await registerUserService(validatedFields.data);
+  const response = await registerUserService(validatedFields.data)
 
-    if (!response || response.error) {
-        return {
-            success: false,
-            message: 'Registration error',
-            strapiErrors: response?.error,
-            zodErrors: null,
-            data: fields,
-        }
+  if (!response || response.error) {
+    return {
+      success: false,
+      message: "Registration error",
+      strapiErrors: response?.error,
+      zodErrors: null,
+      data: fields
     }
+  }
 
-
-    const cookieStore = await cookies();
-    cookieStore.set('jwt', response.jwt, cookieConfig)
-    redirect('/dashboard');
+  const cookieStore = await cookies()
+  cookieStore.set('jwt', response.jwt, cookieConfig)
+  redirect('/dashboard')
 }
